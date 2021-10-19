57deaffbb330d7633f6fcc445d914cebad6007c2
shreetuition <info@shreetuitioncenter.com>
2021-10-19 14:06:14 +0530

    Done

--- index.py
+++ index.py
@@ -1,1 +1,141 @@
-print("enter the number:")
+# 1. camalecase
+# 2. snake_case
+# 3. pascale
+
+# A variable name must start with a letter or the underscore character.
+# A variable name cannot start with a number.
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+# [A-z]
+# [0-9]
+# _ 
+
+# start number
+
+#descritive
+
+# age = 50
+
+# print(AGE)
+
+
+# a = 20
+
+
+# ageofuser
+
+# 3
+
+# age 
+# of 
+# user 
+
+
+# ageOfUser = 20
+# age_of_user = 20 
+
+# AgeOfUser = 20
+
+
+# age = 23
+
+# newAge = str(age)
+
+# # print(type(age))
+
+# print("My Age is "+newAge+" years")
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+
+# z = x.intersection(y)
+
+# print(z)
+
+
+# setA = []
+# setB = []
+
+# for x in range(1,32, 2):
+# 	setA.append(x)
+
+
+# for y in range(1,32, 3):
+# 	setB.append(y)
+
+# setA = set(setA)
+# setB = set(setB)
+# # print(setA)
+# # print(setB)
+
+# z = setA.intersection(setB)
+# print(z)
+
+#operators
+# +
+# -
+# *
+# /
+# %
+
+# =
+# ==
+# >
+# <
+# >=
+# <=
+
+# ===
+# +=
+# -=
+# *=
+# /=
+
+
+# Pre Increment x = ++x
+# Post Increment x++
+
+# True 
+# False
+
+# and 
+# or
+
+# not
+
+x = False
+y = 8
+
+# print(x+y) # 13
+# print(x-y) # -3
+# print(x*y) # 40
+# print(x/y) # 0...
+# print(x%y) # 
+
+
+# print(x==y) # false
+# print(x>y) # true 
+# print(x<y) # false
+# print(x>=y) # true
+# print(x<=y) # false
+
+# x *= x  # x = x+x
+# print(x)
+
+# print(x>5 and y>16)
+# print(x>5 or y>16)
+
+# print(not x)
+
+
+
+# if(not y):
+# 	print("Hello")
+
+
+# 13
+# -3
+# 40
+# 0.625
+# 5
