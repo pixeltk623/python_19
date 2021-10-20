c44a9e39c0578c96c8aa15c1e76af8fc89b454c3
shreetuition <info@shreetuitioncenter.com>
2021-10-20 18:10:15 +0530

    Done

---
+++ string.py
@@ -0,0 +1,141 @@
+# a = 5
+
+# a = 6
+# print(a) # 5
+# print(a) # 6
+
+
+# a variable is a value that can change,
+# depending on conditions or on information passed to the program.
+
+
+# newString = "Test"
+
+
+
+
+
+# print("t" in newString)
+
+# for x in newString:
+# 	print(x)
+
+# print(newString[0])
+
+# print(len(newString))
+
+# for x in xrange(1,10):
+# 	pass
+
+# print(newString.lower())
+
+# print(newString.upper())
+
+# a = 5
+
+# if a>2:
+# 	print(a)
+
+# a = 8
+
+# if a>12:
+# 	print("Hello")
+# else:
+# 	print("Hi")
+
+# x = input("Enter The First Number: ")
+# y = input("Enter The Second Number: ")
+# z = input("Enter The Third Number: ")
+
+# if x>=y and x>=z:
+# 	print(x, "is greater Number")
+# elif y>=x and y>=z:
+# 	print(y, "is greater Number")
+# else:
+# 	print(z, "is greater Number")
+
+
+# p = int(input("Enter The Principle Amount: "))
+# r = int(input("Enter The Rate: "))
+# t = int(input("Enter The Time: "))
+
+# ROI = (p*r*t)/100
+
+# totalAmount = p + ROI
+
+# print(ROI)
+# print(totalAmount)
+
+
+# for loop
+
+# while loop
+
+# a loop is a sequence of instruction s
+# that is continually repeated until a certain condition is reached.
+
+
+# for i in range(1,11):
+# 	print(i)
+
+
+# for i in range(1,11):
+# 	print("Krishna")
+
+
+# 1 - 10 
+
+# 5 6 
+
+# i = 1
+# while i<=10:
+# 	print(i)
+# 	# i = i + 4
+# 	i += 1
+
+
+# break
+
+# continue
+
+# for x in range(1,10):
+# 	if x==5:
+# 		break
+
+# 	print(x)
+
+
+# for x in range(1,10):
+# 	if x==5 or x == 8:
+# 		continue
+
+# 	print(x)
+
+
+# while True:
+# 	name = input("Enter Your Name")
+
+# 	if name=='stop':
+# 		break
+
+
+# 	age = int(input("Enter The Age"))
+
+# 	print("Hello", name, " = ", age)
+
+
+
+# Python Program to Find the Largest Among Three Numbers
+# Python Program to Calculate the Area of a Triangle
+# Python Program to Swap Two Variables
+# Python Program to Convert Celsius To Fahrenheit
+# Python Program to Convert Kilometers to Miles
+# Python Program to Check Prime Number
+# Python Program to Print all Prime Numbers in an Interval
+# Python Program to Find the Factorial of a Number
+# Python Program to Display the multiplication Table
+# Python Program to Print the Fibonacci sequence
+# Python Program to Check Armstrong Number
+# Python Program to Find Armstrong Number in an Interval
+# Python Program to Find the Sum of Natural Numbers
+
