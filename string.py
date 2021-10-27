e5fef5256594f6c0b766ea3467af7df98f1b6919
krishna13324 <krishnarochani@gmail.com>
2021-10-27 17:07:01 +0530

    Update string.py

--- string.py
+++ string.py
@@ -315,4 +315,14 @@
 # 		n=n-1
 # 		print("the sum is",sum)
 
-print bool(3)
+# start=int(input())
+# end=int(input())
+# for i in range(start,end+1):
+#     if i>1:
+#         for j in range(3,i):
+#             if (i%j==0):
+#                 break
+#             else:
+#                 print(i)
+                
+             
