3ff23d1f8d29edc477eab9262e1f77c5cd83fcab
shreetuition <info@shreetuitioncenter.com>
2021-11-19 17:42:40 +0530

    Done

---
+++ dict.py
@@ -0,0 +1,52 @@
+# myDict = {
+# 	"name" : "Sharvan",
+# 	"email" : "s@gmail.com",
+# 	"mobile": 9835411515,
+# 	"status": True
+# }
+
+# print(myDict)
+# print(type(myDict))
+
+# print(myDict['name'])
+# print(len(myDict))
+
+# x = myDict.get("name")
+
+# print(x)
+
+# x = myDict.keys()
+
+# print(x)
+
+# v = myDict.values()
+
+# print(v)
+
+# print(myDict)
+
+# myDict['address'] = "Vadodara"
+# myDict['name'] = "Sanjev"
+
+# print(myDict)
+
+# x = myDict.items()
+
+# print(x)
+
+
+# if "status" in myDict:
+# 	print("yes")
+
+
+myDict = {
+	"name" : "Sharvan",
+	"email" : "s@gmail.com",
+	"mobile": 9835411515,
+	"status": True
+}
+
+
+myDict.update({"color":"red"})
+
+print(myDict)
---
+++ set.py
@@ -0,0 +1,119 @@
+# set1 = {"Test",5,True,"Test"}
+
+
+# print(len(set1))
+# print(type(set1))
+# print(set1) 
+
+# set1 = {"Hello", "Sharvan", "Kumar"}
+
+# print(set1)
+
+
+# set1 = set({"Hello", "Sharvan", "Kumar"})
+
+# print(set1)
+
+# print(type(set1))
+
+# set1 = {"Hello", "Sharvan", "Kumar"}
+
+# for x in set1:
+# 	print(x)
+
+
+# print("HelloS" in set1)
+
+# set1.add("Krishna")
+
+# test = set1.add("Krishna")
+
+
+# print(set1)
+
+# print(test)
+
+# set1 = {"Hello", "Sharvan", "Kumar"}
+# set2 = {"Sanjeev", "Patel", "Rohit"}
+# list1 = [4,5,5,5,"asdasd"]
+# set1.update(set2)
+
+# set1.update(list1)
+
+
+# print(set1)
+
+
+
+# set1 = {"Hello", "Sharvan", "Kumar"}
+# set2 = {"Sanjeev", "Patel", "Rohit"}
+# list1 = [4,5,5,5,"asdasd"]
+
+# set1.remove("Sharvan")
+
+# print(set1)
+
+# set1.remove("Sanjeev")
+
+# print(set1)
+
+# set1.discard("Sanjeev")
+
+# print(set1)
+
+
+set1 = {"Hello", "Sharvan", "Kumar"}
+set2 = {"Sanjeev", "Patel", "Rohit"}
+list1 = [4,5,5,5,"asdasd"]
+
+# print(set1)
+# set3 = set1.pop()
+
+# print(set3)
+# print(set1)
+
+# set1.clear()
+
+# print(set1)
+
+# del set1
+
+# print(set1)
+
+
+# set1 = {"Patel", "Sharvan", "Kumar"}
+# set2 = {"Sanjeev", "Patel", "Rohit"}
+
+
+# set3 = set1.union(set2)
+
+
+# print(set3)
+
+
+# set1.update(set2)
+
+# print(set1)
+
+
+
+# set1.intersection_update(set2)
+
+# print(set1)
+
+
+# set3 = set1.intersection(set2)
+
+# print(set3)
+
+
+set1 = {"Patel", "Sharvan", "Kumar"}
+set2 = {"Sanjeev", "Patel", "Rohit"}
+
+# set1.symmetric_difference_update(set2)
+
+# print(set1)
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3)
