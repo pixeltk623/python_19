f315c446c7458eb685ab26fbafa48cb62cbdfc77
shreetuition <info@shreetuitioncenter.com>
2021-11-30 13:25:46 +0530

    Done

---
+++ hello.py
@@ -0,0 +1,22 @@
+# class Person:
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+
+#     def __str__(self):
+#         return f'Person({self.first_name},{self.last_name},{self.age})'
+
+
+# ob = Person("Sharvan","Kumar",55)
+
+# print(ob)
+
+# class A:
+
+#     def stackoverflow(self, i='some_default_value'):
+#         print('only method')
+
+# ob=A()
+# ob.stackoverflow(2)
+# ob.stackoverflow()
--- oop.py
+++ oop.py
@@ -1,52 +1,189 @@
-# class MyClass:
-# 	x = 5
+# # class MyClass:
+# # 	x = 5
 
-# objectOClass = MyClass()
+# # objectOClass = MyClass()
 
-# #print(objectOClass)
+# # #print(objectOClass)
 
-# print(objectOClass.x)
+# # print(objectOClass.x)
 	
 
-# class Person:
-# 	def __init__(self,name,email,mobile):
+# # class Person:
+# # 	def __init__(self,name,email,mobile):
+# # 		self.name = name;
+# # 		self.email = email;
+# # 		self.mobile = mobile;
+
+# # 	def getMyFullname(self):
+# # 		print("Hello This is "+self.name+" kumar")
+
+# # objectOClass = Person("Sanjeev", "s@gmail.com", 9835401515)
+
+# # # del objectOClass
+
+
+# # objectOClass.name = "Raushan"
+	
+# # # print(objectOClass.name)
+# # # print(objectOClass.email)
+# # # print(objectOClass.mobile)
+
+# # objectOClass.getMyFullname()
+
+
+# # class Manager:
+# # 	def __init__(self, fname, email):
+# # 		self.fname = fname
+# # 		self.email = email
+
+# # 	def printManagerName(self):
+# # 		print(self.fname, self.email)
+
+# # class Employee(Manager):
+# # 	def __init__(self, fname, email):
+# # 		Manager.__init__(self,fname,email)
+
+# # 	def getMyAddress(self):
+# # 		return "Gotri"
+
+# # objectOClass = Employee("Sanjeev", "s@gmail.com")
+
+# # objectOClass.printManagerName()
+# # print(objectOClass.getMyAddress())
+
+# # class ClassName(object):
+# # 	"""docstring for ClassName"""
+# # 	def __init__(self, arg):
+# # 		super(ClassName, self).__init__()
+# # 		self.arg = arg
+
+# class Company():
+	
+# 	def __init__(self, name, email, mobile):
+# 		self.name = name
+# 		self.email = email
+# 		self.mobile = mobile
+
+# 	def getName(self):
+# 		print(self.name)
+
+# class Employee(Company):
+# 	__newSalary=54564
+# 	def __init__(self, name, email, mobile):
+# 		Company.__init__(self,name,email,mobile)
+# 		self._salary = "5456465"
+
+# 	def getMobileNumber(self):
+# 		print(self.mobile)
+
+# 	def getSalary(self):
+# 		print(self._salary)
+# 		# super(ClassName, self).__init__()
+# 		# self.arg = arg
+
+# 	def getNewSalary(self):
+# 		print(self.__newSalary)
+		
+
+# # objectT = Company("Sharvan", "s@gmail.com","5465465")
+
+# # print(objectT)
+
+# # objectT.getName()
+
+# objectT = Employee("Sharvan", "s@gmail.com","5465465")
+# # print(objectT)
+# # objectT.getName()
+# # objectT.getMobileNumber()
+
+
+# # objectT.getSalary()
+
+# # print(objectT.__newSalary)
+
+# objectT.getNewSalary()
+
+
+# class A():
+# 	def __init__(self,name):
 # 		self.name = name;
-# 		self.email = email;
-# 		self.mobile = mobile;
 
-# 	def getMyFullname(self):
-# 		print("Hello This is "+self.name+" kumar")
+# class B(A):
+# 	def __init__(self, name):
+# 		super(A, self).__init__()
+# 		self.name = name
 
-# objectOClass = Person("Sanjeev", "s@gmail.com", 9835401515)
+# 	def getName(self):
+# 		print(self.name)
 
-# # del objectOClass
+# objectT = B("Kumar")
 
+# # print(objectT)
 
-# objectOClass.name = "Raushan"
-	
-# # print(objectOClass.name)
-# # print(objectOClass.email)
-# # print(objectOClass.mobile)
-
-# objectOClass.getMyFullname()
+# objectT.getName()
 
 
-class Manager:
-	def __init__(self, fname, email):
-		self.fname = fname
-		self.email = email
+# class A():
+# 	def __init__(self, name='Kumar'):
+# 		self.name = name
 
-	def printManagerName(self):
-		print(self.fname, self.email)
+# 	def __str__(self):
+# 		return self.name
 
-class Employee(Manager):
-	def __init__(self, fname, email):
-		Manager.__init__(self,fname,email)
+# objectT = A()
+# objectT2 = A("Sharvan")
+# objectT3 = A("Krishna")
+# objectT4 = A()
 
-	def getMyAddress(self):
-		return "Gotri"
+# print(objectT)
+# print(objectT2)
+# print(objectT3)
+# print(objectT4)
+		
 
-objectOClass = Employee("Sanjeev", "s@gmail.com")
+# myTupe = ("Sharvan","Rahul","Kumar")
 
-objectOClass.printManagerName()
-print(objectOClass.getMyAddress())
+# print(myTupe)
+
+# newvar = iter(myTupe)
+
+# print(next(newvar))
+# print(next(newvar))
+# print(next(newvar))
+# print(next(newvar))
+
+# myTupe = "Sharvan"
+
+# print(myTupe)
+
+# newvar = iter(myTupe)
+
+# print(next(newvar))
+# print(next(newvar))
+# print(next(newvar))
+# print(next(newvar))
+
+class MyNumbers:
+	def __iter__(self):
+		self.a = 1
+
+		return self;
+
+	def __next__(self):
+		x = self.a
+		self.a += 1
+		return x
+
+myclass = MyNumbers()
+
+myIter = iter(myclass)
+
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
